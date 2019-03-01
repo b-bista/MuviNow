e9de228787419c45ef72ecf9a28afabb8d20be44
Bikranta <bikranta@bibhusas-macbook-air.local>
2019-02-28 22:06:44 -0500

    Flixter Part 2

---
+++ MuviNow/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  MuviNow
+//
+//  Created by Bikranta on 2/28/19.
+//  Copyright © 2019 Bikranta. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
